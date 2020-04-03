4fa69553b3d1064a409a5d1ae955a73b66895611
Koray Ozkal <koryozkal@gmail.com>
2020-04-03 00:10:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ return  ++ n
 function decrement(n) {
 return -- n 
 }
-function parseInt(n){
-return parseInt('n', 10)
+function makeInt(n) {
+return parseInt( 'n', 10)
 }
 function preserveDecimal(n){
 return parseFloat(n)
